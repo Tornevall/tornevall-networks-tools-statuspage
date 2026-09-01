dea8e9c03daca4acfc88ed5d83561442583fd26a
Thomas Tornevall <thorne@tornevall.net>
2026-09-01 07:55:37 +0200

    Separate Vite and Vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import react from '@vitejs/plugin-react';
-import { loadEnv } from 'vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), 'VITE_');
 
   return {
-    base: env.VITE_BASE_PATH || '/',
+    base: env.VITE_BASE_PATH || './',
     plugins: [react()],
-    test: {
-      environment: 'jsdom',
-      globals: true,
-      setupFiles: './src/test/setup.ts',
-      css: true,
-    },
   };
 });
